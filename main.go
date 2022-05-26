3ae91b074e90d0e5c81fca364913e31354250be2
vital <vitalikgk@mail.ru>
2022-05-26 22:50:44 +0400

    test

---
+++ main.go
@@ -0,0 +1,25 @@
+package golang_united_school_homework
+
+import (
+	"fmt"
+)
+
+// func (c *Circle) CalcPerimeter() float64 {
+// 	return c.Radius * 2
+// }
+
+// func (c *Circle) CalcArea() float64 {
+// 	return c.Radius * 4
+// }
+
+func main() {
+	//var c Circle
+	//c.Radius = 2
+	//var b1 = NewBox(5)
+	//b1.AddShape()
+	//b1 := make([]Shape, 10)
+	var b box
+	//b.AddShape(c)
+	//b.shapes = append(b1, c)
+	fmt.Println("B=")
+}
